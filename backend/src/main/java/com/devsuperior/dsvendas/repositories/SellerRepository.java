7b682597a465ad91f740cf3e8ca553f770c19ea1
Diego <dcandido@ciandt.com>
2021-05-06 12:53:45 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.devsuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendas.entites.Seller;
+
+/**
+ * @author dcandido
+ *
+ */
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
