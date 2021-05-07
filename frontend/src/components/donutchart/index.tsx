d80a8f77835a0c18f713297e97429c709b247b4b
Diego <dcandido@ciandt.com>
2021-05-07 16:51:01 -0300

    DonutChart integration

--- frontend/src/components/donutchart/index.tsx
+++ frontend/src/components/donutchart/index.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,23 +12,25 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    let chartData : ChartData = { labels:[], series:[] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[]
-            const myLabels = data.map(x => x.sellerName)
-            const mySeries = data.map(x => x.sum)
-            
-            chartData = {labels: myLabels, series: mySeries}
-            console.log(chartData)
-        })
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[]
+                const myLabels = data.map(x => x.sellerName)
+                const mySeries = data.map(x => x.sum)
+
+                setChartData({ labels: myLabels, series: mySeries })
+            })
+    }, [])
+
 
     // const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
     //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     // }
-    
+
     const options = {
         legend: {
             show: true
@@ -34,13 +38,13 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{...options, labels:chartData.labels}} 
-            series={chartData.series} 
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
     );
-  }
-  
-  export default DonutChart; 
+}
+
+export default DonutChart;
